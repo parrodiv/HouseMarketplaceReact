d827d72e36f4a1dc6ed3e0b7ffd8e3bf80dc07bc
Alessandro Parrilla <paggio96@gmail.com>
2022-05-07 17:30:20 +0200

    installed react-router-dom and Pages & Routes created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
-    
   );
 }
 
