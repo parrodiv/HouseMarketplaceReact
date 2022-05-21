2eb95b554d8aa1aaa39a636bcb79045372912126
Alessandro Parrilla <paggio96@gmail.com>
2022-05-21 15:33:46 +0200

    Uploading Images to Firebase Storage (not yet in firestore database)

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,7 +1,16 @@
 import { useState, useEffect } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
+import {db} from '../firebase.config'
 import { useNavigate } from 'react-router-dom';
 import Spinner from '../components/Spinner';
+import {v4 as uuidv4} from 'uuid'
+// allow to create a unique id
 import { toast } from 'react-toastify';
 
 const initialFormState = {
@@ -124,8 +133,67 @@ function CreateListing() {
       location = address;
     }
 
+    // STORE IMAGE IN FIREBASE STORAGE
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage()
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+        const storageRef = ref(storage, 'images/' + fileName)
+        console.log(storageRef);
+
+        const uploadTask = uploadBytesResumable(storageRef, image)
+
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused');
+                break;
+              case 'running':
+                console.log('Upload is running');
+                break;
+            }
+          },
+          (error) => {
+            reject(error)
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+              //it will return a downloadURL for each image
+            });
+          }
+        );
+      })
+    }
+
+    //The storeImage function only stores one image at a time, so in order to call it multiple times and essentially do something when they are all uploaded you would need the storeImage function to return a Promise, that you can await the resolution of somewhere else when you use the function.
+
+    console.log(images);
+    const imgUrls = await Promise.all(
+      // loop trough images array
+      [...images].map(image => storeImage(image))
+    ).catch(() => {
+      setLoading(false)
+      toast.error('Images not uploaded')
+      return
+    })
+    
+
+    console.log(imgUrls);
+
     setLoading(false);
   };
+  
 
   const onMutate = (e) => {
     let boolean = null;
@@ -142,7 +210,7 @@ function CreateListing() {
     if (e.target.files) {
       setFormData((prevState) => ({
         ...prevState,
-        images: e.target.files, //arr of images
+        images: e.target.files, //list of images similar to arr
       }));
     }
     // console.log(e.target.files);
