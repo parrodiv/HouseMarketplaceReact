16dcbd51ff514ee010658ea7f74fb84a28b8ea6c
Alessandro Parrilla <paggio96@gmail.com>
2022-05-26 14:44:57 +0200

    Listing Page Slider with SwiperJs

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,6 +1,16 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/a11y';
+
 import { getDoc, doc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { db } from '../firebase.config';
@@ -40,7 +50,25 @@ function Listing() {
 
   return (
     <main>
-      {/*SLIDER*/}
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+      >
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <img
+              style={{ width: '100%', height: '450px' }}
+              src={url}
+              alt="{listing.title}"
+              size='cover'
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <div
         className="shareIconDiv"
